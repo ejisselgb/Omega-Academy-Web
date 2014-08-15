797a878a369dc411696ad8c920d3dec383079af9
Frank Edward Daza González <frank.daza2@gmail.com>
2014-08-15 11:20:05 -0500

    Corregido banners

--- public/js/ieee.js
+++ public/js/ieee.js
@@ -8,4 +8,13 @@ function decimal2(str) {
   }        
   xmlhttp.open("GET","../../../controllers/estandarIEEE/getSign.php?decimal="+str,true);
   xmlhttp.send();
+
+  var xmlhttp2 = new XMLHttpRequest();
+  xmlhttp2.onreadystatechange = function() {
+    if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {            
+      document.getElementById("binary").value = xmlhttp2.responseText;            
+    }
+  }        
+  xmlhttp2.open("GET","../../../controllers/estandarIEEE/getBinary.php?decimal="+str,true);
+  xmlhttp2.send();
 }
