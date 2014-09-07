4f2ae0af462f305722fa525d83960dbd65a8c879
Frank Edward Daza González <frank.daza2@gmail.com>
2014-09-06 22:35:02 -0500

    Creado metodoBiseccion.js -> borrar()

---
+++ public/js/metodoBiseccion.js
@@ -0,0 +1,8 @@
+function borrar() {
+	document.getElementById('funcion').value = "";
+	document.getElementById('a').value = "";
+	document.getElementById('b').value = "";
+	document.getElementById('digitos').value = "";
+	document.getElementById('raiz').value = "";
+	document.getElementById('error').value = "";
+}
