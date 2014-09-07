98c9a7a54d702f51d039a4a7204c317718ad391f
Frank Edward Daza González <frank.daza2@gmail.com>
2014-09-07 13:57:06 -0500

    Agregado método expression()

--- models/metodoBiseccion/Bisection.php
+++ models/metodoBiseccion/Bisection.php
@@ -9,6 +9,8 @@ require "../validadorExpresiones/Evaluar.php";
 */
 class Bisection
 {
+	// Función a evaluar
+	private $func;
 	// Límite inferior
 	private $a;
 
@@ -18,19 +20,82 @@ class Bisection
 	/**
 	* Método constructor.
 	* Almacena los límites superior e inferior en la variables $a y $b.
-	* @param float $a | $b
+	* @param string $func - float $a | $b 
 	*/
-	function __construct($a, $b) {
+	function __construct($func, $a, $b) {
+		$this->func = $func;
 		$this->a = $a;
 		$this->b = $b;
 	}
 
 	/**
+	* Devuelve el resultado de una expresión algebraica.
+	* @param float $value
+	* @return $result | false resultado de evaluar la expresión con $value, false
+	* si la expresión no existe.
+	*/
+	public function expression($value) {
+		$evaluadorExpresiones = new Evaluar();
+		$exprAlgebraica = $this->func;
+
+		// Convierto toda la expresión a letras minúsculas
+	  $exprAlgebraica = strtolower($exprAlgebraica);
+
+	  // Cambio los nombres de algunas funciones según el estándar internacional.
+	  $exprAlgebraica = str_replace("ln(", "log(", $exprAlgebraica);
+	  $exprAlgebraica = str_replace("arcsen(", "asn(", $exprAlgebraica);
+	  $exprAlgebraica = str_replace("arcsin(", "asn(", $exprAlgebraica);
+	  $exprAlgebraica = str_replace("arccos(", "acs(", $exprAlgebraica);
+	  $exprAlgebraica = str_replace("arctan(", "atn(", $exprAlgebraica);
+
+	  //Quita espacios, tabuladores, encierra en paréntesis, vuelve a minúsculas
+  	$Transformado = $evaluadorExpresiones->TransformaExpresion($exprAlgebraica);
+
+  	//Chequea la sintaxis de la expresión
+	  $chequeoSintaxis = $evaluadorExpresiones->EvaluaSintaxis($Transformado);
+	  if ($chequeoSintaxis == 0) //Si la sintaxis es correcta
+	  {
+	    //Transforma la expresión para aceptar los menos unarios agregando (0-1)#
+	    $ExprNegativos = $evaluadorExpresiones->ArreglaNegativos($Transformado);   
+
+	    //Analiza la expresión
+	    $evaluadorExpresiones->Analizar($ExprNegativos);
+
+	    //Da valor a las variables    
+	    $evaluadorExpresiones->ValorVariable('x', $value); 
+
+	    //Evalúa la expresión para retornar un valor
+	    $valor = $evaluadorExpresiones->Calcular();
+
+	    //Si hay un fallo matemático se captura con este si condicional
+	    if (is_nan($valor) || is_infinite($valor)) {
+	      return false;
+	    }
+	    else { //No hay fallo matemático, se muestra el valor
+	      return $valor;
+	    }
+	  }
+	}
+
+	/**
 	* Verifica si existe una raíz en el intervalo.
+	* @return boolean true si existe, false de lo contrario.
 	*/
 	public function root_exists() {
-
+		// Evaluación límite inferior y superior.
+		if ($this->expression($this->a) && $this->expression($this->b))
+			return true;		
+		else
+			return false;		
 	}
 }
+
+
+$h = new Bisection("ln(x)", -12, 12);
+if ($h->root_exists()) {
+	echo "Existe la raíz";
+}
+else
+	echo "No existe !!!!";
 
 ?>
